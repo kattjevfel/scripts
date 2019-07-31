3f5d32807bd849c8142b955647344f2b5da81d38
Magnus Boman <Kattus@users.noreply.github.com>
2019-07-31 21:34:21 +0200

    update for 4.11

--- d9vk-winelib_steam.sh
+++ d9vk-winelib_steam.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-# Script for installing d9vk into Steam Proton 4.2 on arch(-based distros)
+# Script for updating d9vk on Steam Proton on arch(-based distros)
 #
 # For use with https://aur.archlinux.org/packages/d9vk-winelib-git
 #   or non-git https://aur.archlinux.org/packages/d9vk-winelib
 
-protonpath="$HOME/.local/share/Steam/steamapps/common/Proton 4.2"
+protonpath="$HOME/.local/share/Steam/steamapps/common/Proton 4.11"
 
-# Move to the proton directory (or exit if it fails)
-cd "$protonpath" || exit
-
-# Create the required directories
-mkdir "$protonpath/dist/lib/wine/d9vk/"
-mkdir "$protonpath/dist/lib64/wine/d9vk/"
+# Backup original files
+mv "$protonpath/dist/lib64/wine/dxvk/d3d9.dll" "$protonpath/dist/lib/wine/dxvk/d3d9.dll.bak"
+mv "$protonpath/dist/lib64/wine/dxvk/d3d9.dll" "$protonpath/dist/lib64/wine/dxvk/d3d9.dll.bak"
 
 # Install the 32 bit and 64 bit dlls
-ln -s "/usr/share/d9vk/x32/d3d9.dll.so" "$protonpath/dist/lib/wine/d9vk/d3d9.dll"
-ln -s "/usr/share/d9vk/x64/d3d9.dll.so" "$protonpath/dist/lib64/wine/d9vk/d3d9.dll"
-
-# Download and apply the d9vk patch to proton
-curl -sL https://github.com/Joshua-Ashton/d9vk/files/3164526/proton-4.2-d9vk.patch.txt | patch -p1 -t
+ln -s "/usr/share/d9vk/x32/d3d9.dll.so" "$protonpath/dist/lib/wine/dxvk/d3d9.dll"
+ln -s "/usr/share/d9vk/x64/d3d9.dll.so" "$protonpath/dist/lib64/wine/dxvk/d3d9.dll"
