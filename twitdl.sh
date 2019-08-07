1728857151184fd69a416c8796e43368d72179fe
Magnus Boman <Kattus@users.noreply.github.com>
2019-08-07 16:27:53 +0200

    add twitter post downloader

---
+++ twitdl.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Downloads all images from twitter post
+
+# Stop if user is mentally challenged
+if [ $# -eq 0 ]
+then
+    echo "Usage: $0 *twitter post*"
+    exit 1
+fi
+
+# Extract highest quality images from post
+pics=$(curl -s "$1" | grep 'property=\"og:image\"' | awk '{print $3}'| sed -e 's/content="//' -e 's/:large">/?name=orig/' | tr '\n' ' ')
+
+# Download that shit (this fails if you put quotes around the variable, dont ask me why)
+curl --fail --remote-time --remote-name-all $pics
+
+# Rename messed up twitter filenames
+for f in *?name=orig; do
+    mv "$f" "${f%?name=orig}"
+done
