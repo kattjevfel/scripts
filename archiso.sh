dc9e8223811e0213f6e42da261d737832ab8bbe4
Magnus Boman <Kattus@users.noreply.github.com>
2019-08-15 00:42:53 +0200

    watch folders are overrated

--- archiso.sh
+++ archiso.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-# Download latest archiso for use with bittorrent watch folder + cron
+# Download latest archiso for use with qBittorrent + cron
+
+# Save path
+path="/mnt/jupiter/OS"
 
 # What day is it today
 today=$(date +%Y.%m.%d)
 
-# Where is the file expected to be
-file="/mnt/jupiter/OS/archlinux-$today-x86_64.iso"
-
-# Where does your torrent client look for torrents
-watch="/mnt/jupiter/etc/qbt/watch/os"
-
-# Check if file exists
-if [ -f "$file" ]; then
+if [ -f "$path/archlinux-$today-x86_64.iso" ]; then
   exit 0
 else
-  # And download if it doesn't, if it's released yet.
-  curl -sfo "$watch/arch.torrent" "https://www.archlinux.org/releng/releases/$today/torrent/"
+  qbittorrent-nox --save-path=$path --category=OS "$(curl -s https://www.archlinux.org/releng/releases/$today/ | grep "magnet:" | grep -o '"[^"]\+"')"
 fi
