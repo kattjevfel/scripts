9546d4cbf69b1c8655c97a778bf51be48fbf2d4c
Magnus Boman <Kattus@users.noreply.github.com>
2020-02-19 22:18:18 +0100

    yiffdl: prettier output

--- yiffdl.sh
+++ yiffdl.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-then
-    echo "This tool downloads full galleries off of yiff.party into your current dir."
-    echo ""
-    echo "Usage: $0 *yiff.party ID*"
-    exit 1
+if [ $# -eq 0 ]; then
+    echo "This tool downloads full galleries off of yiff.party into your current dir.
+
+Usage: $0 [yiff.party ID]"
+    exit
 fi
 
-wget --no-verbose --show-progress --input-file /dev/fd/3 3<<< "$(
-wget -qO- https://yiff.party/"$1".json | \
+wget --quiet --show-progress --input-file /dev/fd/3 3<<< "$(
+    wget -qO- https://yiff.party/"$1".json | \
 
     # Prettify json
     python -mjson.tool | \
@@ -18,5 +17,5 @@ wget -qO- https://yiff.party/"$1".json | \
     grep -Po '"file_url": *\K"[^"]*"' | \
 
     # Delete quotes
-    tr -d '"' \
+    tr -d '"'
 )"
