070d9f0738c8e4effb996e276aa5182ccb5d6bcd
Magnus Boman <Kattus@users.noreply.github.com>
2020-06-24 23:37:51 +0200

    twitdl: work with new design

--- twitdl.sh
+++ twitdl.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+# Downloads all images from a twitter post
 
 if [ $# -eq 0 ]; then
-    echo "Downloads all images from a twitter post
-
-Usage: $0 [twitter post URL]"
+    echo "Usage: $0 [twitter post ID]"
     exit
 fi
 
 wget --quiet --show-progress --input-file /dev/fd/3 3<<<"$(
-    wget -qO- "$1" |
-        grep 'property=\"og:image\"' |
-        grep -Po 'content="*\K"[^"]*"' |
-        sed -e 's/"//' -e 's/:large"/?name=orig/'
+    wget -qO- "https://mobile.twitter.com/i/web/status/$1" |
+        grep ':small' |
+        grep -Po '"*"[^"]*"' |
+        sed -e 's/"//' -e 's/:small"/?name=orig/'
 )"
 
 # Rename messed up twitter filenames
