e562fc46714dfc0757bc08dc70c477f3e53bd1a1
Magnus Boman <magunasu.b97@gmail.com>
2020-07-28 00:58:16 +0200

    update-ytdl: Fix broken script

--- update_ytdl.sh
+++ update_ytdl.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
-# Script to update youtube-dl on Arch Linux when the maintainer is slacking
+# Script to update youtube-dl on Arch Linux
 
 # Find latest version
-latestver=$(curl -s https://api.github.com/repos/ytdl-org/youtube-dl/releases/latest | grep tag_name | cut -d\" -f4)
+latestver=$(curl -s https://api.github.com/repos/ytdl-org/youtube-dl/releases/latest | grep -Po '"tag_name": *"\K[^"]*')
 
 # Quit if already latest version
 if [ "$latestver" = "$(youtube-dl --version)" ]; then
-    echo "youtube-dl is already the latest version, quitting."
+    echo 'youtube-dl is already the latest version, quitting.'
     exit
 fi
-
-# Work dir
-dir=$(mktemp -d)
 
 # Find sha256sum of latest version
 hash=$(curl -sL "https://github.com/ytdl-org/youtube-dl/releases/download/$latestver/SHA2-256SUMS" | grep tar | awk '{print $1;}')
 
-# Download base PKGBUILD
-cd "$dir" || exit
-curl -o PKGBUILD "https://git.archlinux.org/svntogit/community.git/plain/trunk/PKGBUILD?h=packages/youtube-dl"
+# Work dir
+dir=$(mktemp -d)
 
-# Update pkgver, hash and reset pkgrel
-sed -i "s/pkgver="[0-9][0-9][0-9][0-9].[0-9][0-9].[0-9][0-9]"/pkgver=$latestver/g" PKGBUILD
-sed -i "/sha256sums/c\sha256sums=('$hash'" PKGBUILD
-sed -i "/pkgrel/c\pkgrel=1" PKGBUILD
+# Download base PKGBUILD + Update pkgver, hash and reset pkgrel
+cd "$dir" || exit
+curl -s https://raw.githubusercontent.com/archlinux/svntogit-community/packages/youtube-dl/trunk/PKGBUILD |
+    sed -e "s/^pkgver.*/pkgver=$latestver/g" \
+        -e "s/^sha256sums.*/sha256sums=('$hash'/g" \
+        -e "s/^pkgrel.*/pkgrel=1/g" >PKGBUILD
 
 # Package, install and clean up
 makepkg -i
