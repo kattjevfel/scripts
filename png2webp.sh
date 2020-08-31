0e8cb98c57dd95c8437d52d7b280117f51da855b
Magnus Boman <magunasu.b97@gmail.com>
2020-08-31 19:14:40 +0200

    removed useless function

--- png2webp.sh
+++ png2webp.sh
@@ -6,33 +6,31 @@ if [ $# -eq 0 ]; then
 	exit
 fi
 
-converter() {
+for file in "$@"; do
 	# Filesize before converting in bytes & human readable
-	sizepre="$(stat -c '%s' "$1" 2>/dev/null)"
-	sizeprehuman=$(numfmt --to=iec-i --suffix=B --format='%.1f' "$sizepre")
+	sizepre="$(stat -c '%s' "$file" 2>/dev/null)"
+	sizeprehuman=$(numfmt --to=iec-i --suffix=B --format='%.1f' "$sizepre" 2>/dev/null)
 
 	# Print current file (with path) being processed + size in human readable
-	echo -ne "Converting $1 ($sizeprehuman)... "
+	echo -ne "Converting $file ($sizeprehuman)... "
 
 	# Bash can be a nightmare at times, this is how you do STDERR redirects.
 	# ..Moving on, check how the conversion went
-	if ERROR=$({ mogrify -define webp:lossless=true -format webp "$1"; } 2>&1); then
+	if ERROR=$({ mogrify -define webp:lossless=true -format webp "$file"; } 2>&1); then
 		echo 'done!'
 
 		# Filesize after converting in bytes & human readable
-		sizesuf="$(stat -c '%s' "${1%.*}".webp)"
+		sizesuf="$(stat -c '%s' "${file%.*}".webp)"
 		sizesufhuman=$(numfmt --to=iec-i --suffix=B --format='%.1f' "$sizesuf")
 
 		# Show off fancy stats!
-		echo "Converted ${1%.*}.webp ($sizesufhuman) ($(awk "BEGIN {print $sizesuf/$sizepre}")% of original size)"
+		echo "Converted ${file%.*}.webp ($sizesufhuman) ($(awk "BEGIN {print $sizesuf/$sizepre}")% of original size)"
+
+		# Delete original
+		rm "$file"
 	else
 		echo 'failed! :('
 		# Print error and exit
 		echo -e "\033[0;31m$ERROR\033[0m"
-		exit 1
 	fi
-}
-
-for file in "$@"; do
-	converter "$file"
 done
