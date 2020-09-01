0b98e39f45e18206a3c53adb75ea59ffd25d5579
Magnus Boman <magunasu.b97@gmail.com>
2020-09-01 16:54:38 +0200

    add hashify.sh

---
+++ hashify.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+hasher=md5sum
+
+# Fuckup protection
+if [ ! "${PWD##*/}" = "${hasher%sum}" ]; then
+    read -q REPLY\?"Not in ${hasher%sum} directory, proceed?"
+    echo
+    [[ ! $REPLY =~ ^[Yy]$ ]] && exit
+fi
+
+# For all files in current directory with a file extension
+for file in "$PWD"/*.*; do
+    # Rename file to hash_hashsum.ext
+    mv -vn "$file" "${hasher%sum}"_${$($hasher "$file")[1]}."${file##*.}"
+done
