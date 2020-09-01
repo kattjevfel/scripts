f63383ddd918db7588f053e58ee2b56e1fab22d7
Magnus Boman <magunasu.b97@gmail.com>
2020-09-01 14:16:58 +0200

    cleanups

--- fantasyfeeder-dl.sh
+++ fantasyfeeder-dl.sh
@@ -16,21 +16,20 @@ trap cleanup EXIT
 
 for storyid in "$@"; do
     # Get main page
-    wget --quiet --output-document="$tmpfile" "$baseurl/stories/view?id=$storyid"
+    wget -qO "$tmpfile" "$baseurl/stories/view?id=$storyid"
 
     # Get title
     title="$(grep -oP "(?<=<h1 class='title'>).*?(?=</h1>)" "$tmpfile")"
 
     # If we're not in a dir called the submissions title, create and enter it.
-    if [ "${PWD##*/}" != "$title" ]; then
-        echo "Creating and entering directory \"$title\""
-        mkdir -p "$title"
-        cd "$title" || exit 1
+    if [ ! "${PWD##*/}" = "$title" ]; then
+        mkdir -vp "$title"
+        cd "$title" || { echo "Can't enter $title"; continue; }
     fi
 
     # Get cover image
     echo 'Downloading cover image...'
-    wget --quiet --show-progress --no-clobber "$(
+    wget -q --show-progress -nc "$(
         grep -m 1 "Upload/Story/Cover" "$tmpfile" |
             # Get string inside single quotes
             grep -oP "/.+.[a-z]+" |
@@ -42,16 +41,13 @@ for storyid in "$@"; do
     echo "There are $pages pages!"
 
     # Count from 0 to however many pages there are
-    for ((i = 0; i <= ((pages - 1)); i++)); do
-        if [ -f "$i.html" ]; then
-            echo "Page $i already downloaded, skipping..."
+    for ((page = 0; page <= ((pages - 1)); page++)); do
+        if [ -f "$page.html" ]; then
+            echo "Page $page already downloaded, skipping..."
             continue
         fi
-        wget --quiet --show-progress --output-document="$i.tmp" "$baseurl/stories/view?id=$storyid&rowStart=$i"
-        grep subheading -A 1 "$i.tmp" >"$i.html"
-        rm "$i.tmp"
+        wget -q --show-progress -O "$page.tmp" "$baseurl/stories/view?id=$storyid&rowStart=$page"
+        grep subheading -A 1 "$page.tmp" >"$page.html"
+        rm "$page.tmp"
     done
-
-    # Leave directory we created earlier
-    cd ..
 done
