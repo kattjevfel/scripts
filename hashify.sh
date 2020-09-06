256ef9236e6bf54f5a0d006f43e8930e5a81b71b
Magnus Boman <magunasu.b97@gmail.com>
2020-09-06 23:40:17 +0200

    Revert "make hashify work in bash"
    This reverts commit 87fc531a889d18ad5a38b678e014b3d92b10f88b.
    
    Didn't work.

--- hashify.sh
+++ hashify.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 hasher=md5sum
 
 # Fuckup protection
 if [ ! "${PWD##*/}" = "${hasher%sum}" ]; then
-    read -r "Not in ${hasher%sum} directory, proceed?"
+    read -q REPLY\?"Not in ${hasher%sum} directory, proceed?"
     echo
     [[ ! $REPLY =~ ^[Yy]$ ]] && exit
 fi
 
 # For all files in current directory with a file extension
 for file in "$PWD"/*.*; do
-    hashsum=$($hasher "$file")
     # Rename file to hash_hashsum.ext
-    mv -vn "$file" "${hasher%sum}"_"${hashsum[0]}"."${file##*.}"
+    mv -vn "$file" "${hasher%sum}"_${$($hasher "$file")[1]}."${file##*.}"
 done
