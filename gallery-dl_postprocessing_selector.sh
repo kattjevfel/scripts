9dda0d2ad274a7019619ef9614dd56ee6e3d88a0
Magnus Boman <magunasu.b97@gmail.com>
2023-02-03 13:25:51 +0100

    gallery-dl_postprocessing_selector: add support for extracting archives

--- gallery-dl_postprocessing_selector.sh
+++ gallery-dl_postprocessing_selector.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 IFS=$'\n'
+inputfile="${*:2}"
 
 if [ "$1" = png ]; then
-    webpifier.sh "${*:2}" >/dev/null
+    webpifier.sh "$inputfile" >/dev/null
 elif [ "$1" = jpg ] || [ "$1" = jpeg ]; then
-    jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "${*:2}" >/dev/null
+    jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "$inputfile" >/dev/null
+elif [ "$1" = zip ] || [ "$1" = rar ] || [ "$1" = 7z ]; then
+    7zz x "$inputfile" -o"${inputfile%.*}" || exit 1
+    rm "$inputfile"
+    find "${inputfile%.*}" -iname "*.png" -exec webpifier.sh {} \;
+    find "${inputfile%.*}" -iname "*.jpg" -exec jpegoptim -p --workers=max --strip-com --strip-iptc --strip-icc --strip-xmp {} +
 else
     exit 0
 fi
