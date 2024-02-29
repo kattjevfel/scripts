bbc2a7440d811c5ab030e5e83008626946ef5fd3
Magnus Boman <magunasu.b97@gmail.com>
2024-02-29 12:44:29 +0100

    gallery-dl_postprocessing_selector: use 7z if 7zz is not available

--- gallery-dl_postprocessing_selector.sh
+++ gallery-dl_postprocessing_selector.sh
@@ -7,7 +7,13 @@ if [ "$1" = png ]; then
 elif [ "$1" = jpg ] || [ "$1" = jpeg ]; then
     jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "$inputfile" >/dev/null
 elif [ "$1" = zip ] || [ "$1" = rar ] || [ "$1" = 7z ]; then
-    7zz x "$inputfile" -o"${inputfile%.*}" || exit 1
+    if [ -f /usr/bin/7zz ]; then
+        sevenzipprog=7zz
+    else
+        sevenzipprog=7z
+    fi
+
+    $sevenzipprog x "$inputfile" -o"${inputfile%.*}" || exit 1
     rm "$inputfile"
     find "${inputfile%.*}" -iname "*.png" -exec webpifier.sh {} \;
     find "${inputfile%.*}" -iname "*.jpg" -exec jpegoptim -p --workers=max --strip-com --strip-iptc --strip-icc --strip-xmp {} +
