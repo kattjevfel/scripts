1965cd495bdcdc3cac7ca68191b4419399eb9c8d
Magnus Boman <magunasu.b97@gmail.com>
2024-10-16 19:26:10 +0200

    drivetemplist.sh: init

---
+++ drivetemplist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Uses drivetemp kernel module to check drive temps and display their name
+# https://unix.stackexchange.com/a/617450
+
+grep -l "drivetemp" /sys/class/hwmon/hwmon*/name | while read -r f; 
+    do printf "%s: %-.2s°C\n" "$(<"${f%/*}/device/model")" "$(<"${f%/*}/temp1_input")";
+done
