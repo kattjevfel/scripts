f2f6f7e9146d5dcb4d6d63519243b82ed64326d5
Magnus Boman <magunasu.b97@gmail.com>
2024-12-22 18:22:30 +0100

    7zip is now in arch repos as "7z"

--- gallery-dl_postprocessing_selector.sh
+++ gallery-dl_postprocessing_selector.sh
@@ -7,13 +7,7 @@ if [ "$1" = png ]; then
 elif [ "$1" = jpg ] || [ "$1" = jpeg ]; then
     jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "$inputfile" >/dev/null
 elif [ "$1" = zip ] || [ "$1" = rar ] || [ "$1" = 7z ]; then
-    if [ -f /usr/bin/7zz ]; then
-        sevenzipprog=7zz
-    else
-        sevenzipprog=7z
-    fi
-
-    $sevenzipprog x "$inputfile" -o"${inputfile%.*}" || exit 1
+    7z x "$inputfile" -o"${inputfile%.*}" || exit 1
     rm "$inputfile"
     find "${inputfile%.*}" -iname "*.png" -exec webpifier.sh {} \;
     find "${inputfile%.*}" -iname "*.jpg" -exec jpegoptim -p --workers=max --strip-com --strip-iptc --strip-icc --strip-xmp {} +
