bbfe081a82b33170f72b2fdda7d63f4ba414dc5e
Magnus Boman <magunasu.b97@gmail.com>
2025-01-22 19:10:50 +0100

    gallery-dl_postprocessing_selector: Work around https://github.com/ip7z/7zip/pull/36

--- gallery-dl_postprocessing_selector.sh
+++ gallery-dl_postprocessing_selector.sh
@@ -7,7 +7,7 @@ if [ "$1" = png ]; then
 elif [ "$1" = jpg ] || [ "$1" = jpeg ]; then
     jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "$inputfile" >/dev/null
 elif [ "$1" = zip ] || [ "$1" = rar ] || [ "$1" = 7z ]; then
-    7z x "$inputfile" -o"${inputfile%.*}" || exit 1
+    LC_CTYPE=ja_JP.UTF-8 7z x "$inputfile" -o"${inputfile%.*}" || exit 1
     rm "$inputfile"
     find "${inputfile%.*}" -iname "*.png" -exec webpifier.sh {} \;
     find "${inputfile%.*}" -iname "*.jpg" -exec jpegoptim -p --workers=max --strip-com --strip-iptc --strip-icc --strip-xmp {} +
