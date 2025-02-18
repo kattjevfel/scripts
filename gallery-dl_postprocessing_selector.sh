4885952f47725fcae4205788c765c18b4514bf89
Magnus Boman <magunasu.b97@gmail.com>
2025-02-18 18:11:59 +0100

    gallery-dl_postprocessing_selector: Remove fanbox posters

--- gallery-dl_postprocessing_selector.sh
+++ gallery-dl_postprocessing_selector.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 IFS=$'\n'
-inputfile="${*:2}"
+inputfile="${*:3}"
 
 if [ "$1" = png ]; then
     webpifier.sh "$inputfile" >/dev/null
 elif [ "$1" = jpg ] || [ "$1" = jpeg ]; then
-    jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "$inputfile" >/dev/null
+    if [ "$2" = kemonoparty ]; then
+        identify -format "%G %i\n" "$inputfile" 2>/dev/null | grep -Po '1200x630 \K.*' | while IFS= read -r file; do rm -f -- "$file"; done
+    else
+        jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp "$inputfile" >/dev/null
+    fi
 elif [ "$1" = zip ] || [ "$1" = rar ] || [ "$1" = 7z ]; then
     LC_CTYPE=ja_JP.UTF-8 7z x "$inputfile" -o"${inputfile%.*}" || exit 1
     rm "$inputfile"
