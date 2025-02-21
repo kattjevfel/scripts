0d62dae922d1120e365045e32b2670c93a5b473d
Magnus Boman <magunasu.b97@gmail.com>
2025-02-22 00:02:52 +0100

    gallery-dl_postprocessing_selector: Perform some cleaning on archives

--- gallery-dl_postprocessing_selector.sh
+++ gallery-dl_postprocessing_selector.sh
@@ -15,6 +15,9 @@ elif [ "$1" = zip ] || [ "$1" = rar ] || [ "$1" = 7z ]; then
     rm "$inputfile"
     find "${inputfile%.*}" -iname "*.png" -exec webpifier.sh {} \;
     find "${inputfile%.*}" -iname "*.jpg" -exec jpegoptim -p --strip-com --strip-iptc --strip-icc --strip-xmp {} +
+    find "${inputfile%.*}" -type d \( -name ".DS_Store" -o -name "__MACOSX" \) -exec rm -rf {} \;
+    find "${inputfile%.*}" -type f \( -name "Thumbs.db" -o -name ".DS_Store" -o -iname \*.clip -o -iname \*.psd -o -iname \*.psb -o -iname \*.twd -o -iname \*.sai -o -iname \*.blend -o -iname \*.sut \) -delete
+    find "${inputfile%.*}" -type d -empty -delete
 else
     exit 0
 fi
